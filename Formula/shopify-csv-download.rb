69540ce6424c884e5acb98f5a67a384b868e056c
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-22 06:56:25 +0000

    Brew formula update for shopify-csv-download version v0.11.1

--- Formula/shopify-csv-download.rb
+++ Formula/shopify-csv-download.rb
@@ -2,16 +2,16 @@
 class ShopifyCsvDownload < Formula
   desc "Download a shopify site in a csv format that the shopify importer understands"
   homepage "https://github.com/kishaningithub/shopify-csv-download"
-  version "0.11.0"
+  version "0.11.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/kishaningithub/shopify-csv-download/releases/download/v0.11.0/shopify-csv-download_0.11.0_darwin_amd64.tar.gz"
-    sha256 "afbe9302e093ce6b08f64a5993a6db1466a8cccfccbe70bf30d5dfb108ba34f1"
+    url "https://github.com/kishaningithub/shopify-csv-download/releases/download/v0.11.1/shopify-csv-download_0.11.1_darwin_amd64.tar.gz"
+    sha256 "f77d135ce2e1763aaa5e581d857281705d0da2ed9fdc2dd841de28c3fcf56f99"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kishaningithub/shopify-csv-download/releases/download/v0.11.0/shopify-csv-download_0.11.0_linux_amd64.tar.gz"
-      sha256 "36c75c830b9c3f5ea3da995dca14da79c1f4bc51d90075caa4757bc72652bafd"
+      url "https://github.com/kishaningithub/shopify-csv-download/releases/download/v0.11.1/shopify-csv-download_0.11.1_linux_amd64.tar.gz"
+      sha256 "168662af465ad3ddeb70be60ed16e6e60fc9c496f41341ecdfd06f763c2cef67"
     end
   end
 
